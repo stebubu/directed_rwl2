eb1c592bfe23bc2a818f4d1a55b61c713b8b8cdc
stefano bagli <stefano.bagli@gecosistema.it>
2024-12-06 17:47:19 +0100

    Update home1.py

--- home1.py
+++ home1.py
@@ -1,7 +1,7 @@
 # app/Home.py
 import streamlit as st
 import folium
-from streamlit_folium import folium_static
+from streamlit_folium import st_folium
 from folium.plugins import Draw
 import pandas as pd
 import numpy as np
@@ -111,7 +111,7 @@ def display_rainfall_map(geotiff_path):
             colormap.caption = 'Rainfall Intensity (mm)'
             colormap.add_to(m)
 
-            folium_static(m)
+            st_folium(m)
     except Exception as e:
         st.error(f"Error displaying rainfall map: {e}")
 
@@ -195,7 +195,7 @@ def display_flood_map(flood_map_path):
             colormap.caption = 'Flood Depth'
             colormap.add_to(m)
 
-            folium_static(m)
+            st_folium(m)
     except Exception as e:
         st.error(f"Error displaying flood map: {e}")
 
@@ -269,7 +269,7 @@ def create_map_viewer_with_barrier(radar_name, radar_intensity, flood_name, floo
     draw.add_to(m)
 
     folium.LayerControl().add_to(m)
-    folium_static(m)
+    st_folium(m)
 
 # Function to create time series plot for a specific point
 '''def create_time_series(selected_date, point_id):
@@ -483,7 +483,7 @@ def display_cog_with_folium(cog_path):
         colormap.caption = 'Rainfall Intensity'
         colormap.add_to(m)
 
-        folium_static(m)
+        st_folium(m)
 
 # Main navigation
 st.sidebar.title("Navigation")
